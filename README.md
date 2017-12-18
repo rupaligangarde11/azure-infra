f65e0d329716e11c4ebfc6eb667d53ed54235c1d
Rupali Gangarde <rupalig@thoughtworks.com>
2017-12-18 13:57:32 +0530

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# azure-infra
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,15 @@
+name 'azure-infra'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures azure-infra'
+long_description 'Installs/Configures azure-infra'
+version '0.1.0'
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Issues` link
+# issues_url 'https://github.com/<insert_org_here>/azure-infra/issues' if respond_to?(:issues_url)
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Source` link
+# source_url 'https://github.com/<insert_org_here>/azure-infra' if respond_to?(:source_url)
